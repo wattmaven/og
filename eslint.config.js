b35028cf504b40148248a8a34e0c2290ff46fee2
Elijah Passmore <elijah.passmore@wattmaven.com>
2025-03-08 20:06:53 +1300

    feat: add basic setup (#2)

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ["next", "prettier"],
+    ignorePatterns: [".next"],
+  }),
+];
+
+export default eslintConfig;
